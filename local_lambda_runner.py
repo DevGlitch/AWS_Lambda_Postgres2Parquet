50fbe98c5d1a089cae93882df39dabb2bacad328
D̷e̵v̷G̸l̶i̸t̷c̸h̶ <59583446+DevGlitch@users.noreply.github.com>
2023-08-25 13:17:56 -0700

    Add local lambda runner

---
+++ local_lambda_runner.py
@@ -0,0 +1,11 @@
+import json
+from lambda_function import lambda_handler
+
+
+with open('test_event.json', 'r') as event_file:
+    event = json.load(event_file)
+
+context = None  # You can create a mock context object if needed
+
+response = lambda_handler(event, context)
+print(response)
